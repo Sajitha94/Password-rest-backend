c18818bc85ffe22786d8e1258a19069c3bebe7d2
Sajitha94 <sajithasubramani94@gmail.com>
2025-09-14 15:45:56 +0530

    first commit

---
+++ src/config/db.js
@@ -0,0 +1,16 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.status(200).send("Application is working well");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is Running on ${PORT}`); 
+});
